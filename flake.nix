826539d217fc69ef951ec00bdfe6d240d9ddf9b8
Karsten Finderup Pedersen <kpede22@student.sdu.dk>
2024-10-14 16:58:45 +0200

    Setup flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Development environment";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          vscode-langservers-extracted
+          vscode-extensions.astro-build.astro-vscode
+        ];
+      };
+    }
+  );
+}
